3349c62572459e4b898003d3ad8d1cf59b59b2fa
workb <workbydr247@gmail.com>
2024-03-11 22:58:53 +0530

    - commit and push done

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "demo1",
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
